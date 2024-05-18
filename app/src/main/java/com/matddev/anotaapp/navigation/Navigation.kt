a92f00ab8a52bb84e35a4b935f8bc1094cf00608
Matheus <matheusaraujo.ti10@gmail.com>
2024-05-17 22:35:40 -0300

    refactor(feature): remove unused navigation routes

--- app/src/main/java/com/matddev/anotaapp/navigation/Navigation.kt
+++ app/src/main/java/com/matddev/anotaapp/navigation/Navigation.kt
@@ -3,9 +3,6 @@ package com.matddev.anotaapp.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.matddev.file_manager.screens.list_screen.ListScreen
-import com.matddev.file_manager.screens.bookmark_screen.Bookmark
 
 @Composable
 fun Navigation(navHostController: NavHostController) {
@@ -13,7 +10,6 @@ fun Navigation(navHostController: NavHostController) {
         navController = navHostController,
         startDestination = "list",
     ) {
-        composable("list"){ ListScreen() }
-        composable("bookmark"){ Bookmark() }
+
     }
 }
