3610e8a50feb6f217974b0c6944ddaaecacc77fe
Matheus <matheusaraujo.ti10@gmail.com>
2024-05-19 22:33:53 -0300

    refactor:  delete unnecessary files

--- feature/src/main/java/com/matddev/di/AppModules.kt
+++ feature/src/main/java/com/matddev/di/AppModules.kt
@@ -5,25 +5,16 @@ import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
-import androidx.room.Room
 import com.matddev.RepoConstants
-import com.matddev.data_local.data_source.RepoLocalDataSourceImpl
-import com.matddev.data_local.database.Database
-import com.matddev.data_source.RepoLocalDataSource
-import com.matddev.data_source.RepoRemoteDataSource
-import com.matddev.data_source.RepoRemoteDataSourceImpl
 import com.matddev.file_manager.navigation.definition.GithubNavigation
 import com.matddev.file_manager.navigation.implementation.GithubNavigationImpl
-import com.matddev.provideGitHubWebService
-import com.matddev.provideRetrofit
-import com.matddev.repository.RepoRepository
-import com.matddev.repository.RepoRepositoryImpl
+import com.matddev.repository.FileRepository
+import com.matddev.repository.FileRepositoryImpl
 import com.matddev.use_case.GetRepos
 import com.matddev.utils.NavigationManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModules = module {
@@ -37,25 +28,13 @@ val appModules = module {
     }
 }
 
-val databaseModule = module {
-    single {
-        Room.databaseBuilder(
-            androidContext(),
-            Database::class.java,
-            "repository-database"
-        ).build()
-    }
-
-    single { get<Database>().repoDao() }
-}
-
 val dataSourceModules = module {
-    factory<RepoRemoteDataSource> { RepoRemoteDataSourceImpl(get()) }
-    factory<RepoLocalDataSource> { RepoLocalDataSourceImpl(get()) }
+//    factory<RepoRemoteDataSource> { RepoRemoteDataSourceImpl(get()) }
+
 }
 
 val repositoriesModules = module {
-    factory<RepoRepository> { RepoRepositoryImpl(get(), get()) }
+    factory<FileRepository> { FileRepositoryImpl() }
 }
 
 val viewModelModules = module {
@@ -66,8 +45,8 @@ val useCaseModules = module {
 }
 
 val networkModel = module {
-    single { provideRetrofit() }
-    factory { provideGitHubWebService(get()) }
+    single { }
+    factory { }
 }
 
 val navigationModel = module {
