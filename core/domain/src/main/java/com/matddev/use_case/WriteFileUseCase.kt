1b7d56d6c6d17fb788e647d1c602f62a2b14f81f
Matheus <matheusaraujo.ti10@gmail.com>
2024-05-19 22:58:12 -0300

    feat(domain): create use case

--- core/domain/src/main/java/com/matddev/use_case/WriteFileUseCase.kt
+++ core/domain/src/main/java/com/matddev/use_case/WriteFileUseCase.kt
@@ -1,10 +1,12 @@
 package com.matddev.use_case
 
+import android.content.Context
 import com.matddev.repository.FileRepository
 
 class WriteFileUseCase(
     private val repository: FileRepository,
-    private val lines: List<String>
+    private val lines: List<String>,
+    private val context: Context
 ) {
-    operator fun invoke(lines: List<String>) = repository.write(lines)
+    operator fun invoke() = repository.write(lines = lines, context = context)
 }
