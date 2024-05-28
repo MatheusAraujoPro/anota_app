c5dc76c02a09ab7157dc72fbe40c3c16b0957bd2
Matheus <matheusaraujo.ti10@gmail.com>
2024-05-28 11:24:06 -0300

    feat(feature): create the base of triangles sequence component with canvas

--- feature/src/main/java/com/matddev/file_manager/screens/create_files/CreateFile.kt
+++ feature/src/main/java/com/matddev/file_manager/screens/create_files/CreateFile.kt
@@ -1,17 +1,34 @@
 package com.matddev.file_manager.screens.create_files
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.vector.VectorProperty
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.matddev.anotaapp.theme.Theme
+import com.matddev.anotaapp.theme.Theme.colors
 import org.koin.androidx.compose.getViewModel
 
 
@@ -26,27 +43,67 @@ fun CrateFileScreen(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .background(color = colors.background)
+            .fillMaxSize()
     ) {
-        if (viewState.value.result)
-            Text(text = "Deu certo")
-        Button(
-            onClick = {
-                action(
-                    CreateFileAction.CreateFile(
-                        lines = listOf("primeira linha", "segunda linha"),
-                        context = context
-                    )
-                )
-            }
-        ) {
-            Text(
-                text = "Gravar arquiivo",
-                style = Theme.typography.body.copy(
-                    color = Theme.colors.primary
-                )
-            )
+        Triangle(
+            color = colors.success,
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.Start)
+        )
+    }
+}
+
+
+@Composable
+private fun Triangle(
+    color: Color,
+    modifier: Modifier
+) {
+    Canvas(
+        modifier = modifier
+
+    ) {
+        val trianglePath = Path().let {
+//            First line
+            it.moveTo(this.size.width * .20f, this.size.height * .70f)
+            it.lineTo(this.size.width * .30f, this.size.height * 0.50f)
+            it.lineTo(this.size.width * .40f, this.size.height * 0.70f)
+//            Second line
+            it.lineTo(this.size.width * .50f, this.size.height * 0.50f)
+            it.lineTo(this.size.width * .60f, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width * .70f, this.size.height * .50f)
+            it.lineTo(this.size.width * .80f, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width * .90f, this.size.height * .50f)
+            it.lineTo(this.size.width, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width + this.size.width * .10f , this.size.height * .50f)
+            it.lineTo(this.size.width + this.size.width * .20f, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width + this.size.width * .30f , this.size.height * .50f)
+            it.lineTo(this.size.width + this.size.width * .40f, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width + this.size.width * .50f , this.size.height * .50f)
+            it.lineTo(this.size.width + this.size.width * .60f, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width + this.size.width * .70f , this.size.height * .50f)
+            it.lineTo(this.size.width + this.size.width * .80f, this.size.height * 0.70f)
+
+            it.lineTo(this.size.width + this.size.width * .90f , this.size.height * .50f)
+            it.lineTo(this.size.width + this.size.width , this.size.height * 0.70f)
+            
+            it.close()
+            it
         }
+
+        drawPath(
+            path = trianglePath,
+            color = color
+        )
     }
 }
 
