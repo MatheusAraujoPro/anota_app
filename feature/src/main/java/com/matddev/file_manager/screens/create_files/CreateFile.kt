9ff97ef82148adca60e8d51411c5da59929d344f
Matheus <matheusaraujo.ti10@gmail.com>
2024-05-29 19:46:51 -0300

    refactor(feature): create the screen concept

--- feature/src/main/java/com/matddev/file_manager/screens/create_files/CreateFile.kt
+++ feature/src/main/java/com/matddev/file_manager/screens/create_files/CreateFile.kt
@@ -2,32 +2,25 @@ package com.matddev.file_manager.screens.create_files
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.vector.VectorProperty
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.matddev.anotaapp.theme.Theme
 import com.matddev.anotaapp.theme.Theme.colors
 import org.koin.androidx.compose.getViewModel
 
@@ -47,64 +40,111 @@ fun CrateFileScreen(
             .background(color = colors.background)
             .fillMaxSize()
     ) {
-        Triangle(
-            color = colors.success,
+        SequenceOfTrianglesShape(
+            color = colors.backgroundComponent,
             modifier = Modifier
-                .size(100.dp)
+                .height(50.dp)
+                .width(100.dp)
                 .align(Alignment.Start)
+
+        )
+        TextField(
+            value = "",
+            modifier = Modifier
+                .padding(start = 20.dp, end = 12.dp)
+                .height(300.dp)
+                .fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = colors.backgroundComponent,
+                unfocusedContainerColor = colors.backgroundComponent
+            ),
+            onValueChange = {}
         )
     }
 }
 
-
 @Composable
-private fun Triangle(
+private fun SequenceOfTrianglesShape(
     color: Color,
-    modifier: Modifier
+    modifier: Modifier,
+    isRotate: Boolean = false
 ) {
     Canvas(
         modifier = modifier
-
     ) {
         val trianglePath = Path().let {
+            val width = this.size.width
 //            First line
-            it.moveTo(this.size.width * .20f, this.size.height * .70f)
-            it.lineTo(this.size.width * .30f, this.size.height * 0.50f)
-            it.lineTo(this.size.width * .40f, this.size.height * 0.70f)
+            it.moveTo(width * .20f, this.size.height * .70f)
+            it.lineTo(width * .30f, this.size.height * 0.50f)
+            it.lineTo(width * .40f, this.size.height * 0.70f)
 //            Second line
-            it.lineTo(this.size.width * .50f, this.size.height * 0.50f)
-            it.lineTo(this.size.width * .60f, this.size.height * 0.70f)
+            it.lineTo(width * .50f, this.size.height * 0.50f)
+            it.lineTo(width * .60f, this.size.height * 0.70f)
+//            Third Line
+            it.lineTo(width * .70f, this.size.height * .50f)
+            it.lineTo(width * .80f, this.size.height * 0.70f)
+//            Fourth Line
+            it.lineTo(width * .90f, this.size.height * .50f)
+            it.lineTo(width, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width * .70f, this.size.height * .50f)
-            it.lineTo(this.size.width * .80f, this.size.height * 0.70f)
+            it.lineTo(width + width * .10f, this.size.height * .50f)
+            it.lineTo(width + width * .20f, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width * .90f, this.size.height * .50f)
-            it.lineTo(this.size.width, this.size.height * 0.70f)
+            it.lineTo(width + width * .30f, this.size.height * .50f)
+            it.lineTo(width + width * .40f, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width + this.size.width * .10f , this.size.height * .50f)
-            it.lineTo(this.size.width + this.size.width * .20f, this.size.height * 0.70f)
+            it.lineTo(width + width * .50f, this.size.height * .50f)
+            it.lineTo(width + width * .60f, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width + this.size.width * .30f , this.size.height * .50f)
-            it.lineTo(this.size.width + this.size.width * .40f, this.size.height * 0.70f)
+            it.lineTo(width + width * .70f, this.size.height * .50f)
+            it.lineTo(width + width * .80f, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width + this.size.width * .50f , this.size.height * .50f)
-            it.lineTo(this.size.width + this.size.width * .60f, this.size.height * 0.70f)
+            it.lineTo(width + width * .90f, this.size.height * .50f)
+            it.lineTo(width + width, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width + this.size.width * .70f , this.size.height * .50f)
-            it.lineTo(this.size.width + this.size.width * .80f, this.size.height * 0.70f)
+            it.lineTo(width + width + width * .10f, this.size.height * .50f)
+            it.lineTo(width + width + width * .20f, this.size.height * 0.70f)
 
-            it.lineTo(this.size.width + this.size.width * .90f , this.size.height * .50f)
-            it.lineTo(this.size.width + this.size.width , this.size.height * 0.70f)
-            
+            it.lineTo(width + width + width * .30f, this.size.height * .50f)
+            it.lineTo(width + width + width * .40f, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width * .50f, this.size.height * .50f)
+            it.lineTo(width + width + width * .60f, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width * .70f, this.size.height * .50f)
+            it.lineTo(width + width + width * .80f, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width * .90f, this.size.height * .50f)
+            it.lineTo(width + width + width, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width + width * .10f, this.size.height * .50f)
+            it.lineTo(width + width + width + width * .20f, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width + width * .30f, this.size.height * .50f)
+            it.lineTo(width + width + width + width * .40f, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width + width * .50f, this.size.height * .50f)
+            it.lineTo(width + width + width + width * .60f, this.size.height * 0.70f)
+
+            it.lineTo(width + width + width + width * .70f, this.size.height * .50f)
+            it.lineTo(width + width + width + width * .80f, this.size.height * 0.70f)
+            it.lineTo(
+                width + width + width + width * .80f,
+                this.size.height + this.size.height + this.size.height
+            )
+
+            it.lineTo(width * .20f, this.size.height + this.size.height + this.size.height)
+
             it.close()
             it
         }
-
         drawPath(
             path = trianglePath,
             color = color
         )
     }
+
 }
 
 @Preview
