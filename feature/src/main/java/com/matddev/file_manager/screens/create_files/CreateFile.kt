f7e3282564af8dd06bb36ead050fe0ad678e8008
Matheus <matheusaraujo.ti10@gmail.com>
2024-05-30 21:44:49 -0300

    feat(feature): complete the new layout

--- feature/src/main/java/com/matddev/file_manager/screens/create_files/CreateFile.kt
+++ feature/src/main/java/com/matddev/file_manager/screens/create_files/CreateFile.kt
@@ -3,24 +3,34 @@ package com.matddev.file_manager.screens.create_files
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.matddev.anotaapp.theme.Theme
 import com.matddev.anotaapp.theme.Theme.colors
 import org.koin.androidx.compose.getViewModel
 
@@ -33,11 +43,15 @@ fun CrateFileScreen(
     val action = viewModel::dispatcher
     val viewState = viewModel.viewState.collectAsState()
 
+    var descriptionText by remember { mutableStateOf("") }
+    var valueText by remember { mutableStateOf("") }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .background(color = colors.background)
+            .padding(start = 6.dp)
             .fillMaxSize()
     ) {
         SequenceOfTrianglesShape(
@@ -48,26 +62,59 @@ fun CrateFileScreen(
                 .align(Alignment.Start)
 
         )
-        TextField(
-            value = "",
+        Row(
             modifier = Modifier
-                .padding(start = 20.dp, end = 12.dp)
-                .height(300.dp)
+                .padding(start = 20.dp, end = 24.dp)
+                .background(color = colors.backgroundComponent)
+                .fillMaxHeight(0.7f)
                 .fillMaxWidth(),
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = colors.backgroundComponent,
-                unfocusedContainerColor = colors.backgroundComponent
-            ),
-            onValueChange = {}
-        )
+        ) {
+            TextField(
+                value = descriptionText,
+                modifier = Modifier
+                    .weight(3f),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = colors.backgroundComponent,
+                    unfocusedContainerColor = colors.backgroundComponent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
+                textStyle = Theme.typography.body.copy(
+                    textAlign = TextAlign.Start
+                ),
+                onValueChange = { description ->
+                    descriptionText = description
+                }
+            )
+            TextField(
+                value = valueText,
+                modifier = Modifier
+                    .weight(1f),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = colors.backgroundComponent,
+                    unfocusedContainerColor = colors.backgroundComponent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                textStyle = Theme.typography.body.copy(
+                    textAlign = TextAlign.End
+                ),
+                onValueChange = { value ->
+                    valueText = value
+                }
+            )
+        }
     }
 }
 
 @Composable
 private fun SequenceOfTrianglesShape(
     color: Color,
-    modifier: Modifier,
-    isRotate: Boolean = false
+    modifier: Modifier
 ) {
     Canvas(
         modifier = modifier
@@ -147,7 +194,10 @@ private fun SequenceOfTrianglesShape(
 
 }
 
-@Preview
+@Preview(
+    device = Devices.PIXEL_XL,
+    name = "PIXEL"
+)
 @Composable
 private fun CrateFileScreenPreview() {
     CrateFileScreen(
